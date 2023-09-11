fb01cf411ffdcdecc2d0827cc8e95f47b5afbba5
Chris Nitsas <nitsas.chris@gmail.com>
2023-09-11 08:35:46 +0300

    Add 02-values

---
+++ 02-values/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("adding strings: go" + "lang")
+
+	fmt.Println("adding numbers: 1+1 =", 1+1)
+	fmt.Println("dividing: 7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println("boolean and: true && false =", true && false)
+	fmt.Println("boolean or: true || false =", true || false)
+	fmt.Println("boolean not: !true =", !true)
+}
